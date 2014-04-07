6b5134e7788443bae883c07033092dad0e1a4ee4
benjaminrivera <benjamin.rivera@rulesware.com>
2014-04-07 11:01:32 -0500

    code refactor

--- pdc_poc/pdc.js
+++ pdc_poc/pdc.js
@@ -14,52 +14,36 @@ function onRequest(request, response) {
 
  	var db = mongoose.createConnection('mongodb://pdc.rulesware.com/poc');
  	var dbCouchbase  = new couchbase.Connection({host: "pdc.rulesware.com:8091", bucket: 'pdc', password: 'pdc'});  
-	var pdcDB = nano.db.use("pdc_poc");
-  var client = new cql.Client({hosts: ['192.168.212.139:9042'], keyspace: 'test',username:'cassandra',password:'cassandra'});
+	var dbCouchdb = nano.db.use("pdc_poc");
+  var dbCassandra = new cql.Client({hosts: ['192.168.212.139:9042'], keyspace: 'test',username:'cassandra',password:'cassandra'});
 
-  var docs = {
-  'test-doc100': {value: {name: 'Frank00'}},
-  'test-doc200': {value: {name: 'Bob00'}},
-  'test-doc300': {value: {name: 'Jones00'}}
-};
-
-    var a = [];
-    var b= { "test-doc12": { "value": { "name": "Frank" } } };
-    a.push(b);
-    a.push(b)
-
-  if(request.url == "/mongo"){
+  if(request.url == "/mongo")
     getMapping("mongo", function(x){
         db.collection('poc').insert(x,function(obj){
           console.log("data inserted into mongodb!");
         });
     });
-  }
 
-  if(request.url == "/couchdb" ){
+  if(request.url == "/couchdb" )
     getMapping("couchdb", function(x){
-      pdcDB.bulk({"docs": x},function(e,resData){
+      dbCouchdb.bulk({"docs": x},function(e,resData){
         console.log("data inserted into couchdb!");
       });
     });    
-  }
 
-  if(request.url == "/couchbase"){
+  if(request.url == "/couchbase")
     getMapping("couchbase", function(x){
       dbCouchbase.setMulti( x , {}, function(err, results) {
         if (err) console.log(err); else console.log("data inserted into couchbase!");
       });
-    });
-    
-  }
+    });    
 
-  if(request.url == "/cassandra"){
+  if(request.url == "/cassandra")
      getMapping("cassandra", function(x){
-      client.executeBatch (x, 1, {}, function(err) {
+      dbCassandra.executeBatch (x, 1, {}, function(err) {
         if (err) console.log(err); else console.log("data inserted into cassandra!");         
      });
     });
-  }
 
   response.writeHead(200, {"Content-Type": "text/plain"});
 	response.write("write done");
@@ -74,12 +58,7 @@ var getMapping = function(server, callback){
 
       fs.readFile('flow.xml', function(err, data) {
           parser.parseString(data, function (err, result) {
-            if(server == "mongo" || server == "couchdb" )
-              callback(processData(result, processTag));
-            else if( server == "couchbase" )
-                callback(processDataCB(result, processTag));
-            else if( server == "cassandra")
-              callback(processDataCassandra(result, processTag));
+            callback(processData(result, processTag, server));
           });
       });
     } else{
@@ -87,10 +66,10 @@ var getMapping = function(server, callback){
     }
 }
 
-function processData(result, processTag){
+function processData(result, processTag, server){
   var jsonFile =  JSON.stringify(result)  ;
   result = JSON.parse(jsonFile);
-  var temp = [];
+  var temp = getType(server);
 
   var process = result["bpmn2:definitions"]["process"][0]["bpmn2:process"][0];
   _.each(processTag, function(tag){
@@ -108,97 +87,38 @@ function processData(result, processTag){
           })
         });
 
-      var data = {
-        "_id": mongoose.Types.ObjectId(),
-        "id" : Guid.raw(),
-        "metaData" : process[tag][i]["$"],
-        "shapeType" : tag,
-        "hash" : hashInfo,
-        "connectors" : {  "incoming" : process[tag][i]["bpmn2:incoming"] == undefined ? [] : process[tag][i]["bpmn2:incoming"],
-                          "outgoing" : process[tag][i]["bpmn2:outgoing"] == undefined ? [] : process[tag][i]["bpmn2:outgoing"] },
-        "bounds" : bounds["dc:Bounds"]["$"],
-        "metaDiagram" : bounds["$"]
-      }
-      temp.push(data);
+      var _id = mongoose.Types.ObjectId();
+      var data = (server != "cassandra") ? {
+                                              "_id": _id,
+                                              "id" : Guid.raw(),
+                                              "metaData" : process[tag][i]["$"],
+                                              "shapeType" : tag,
+                                              "hash" : hashInfo,
+                                              "connectors" : {  "incoming" : process[tag][i]["bpmn2:incoming"] == undefined ? [] : process[tag][i]["bpmn2:incoming"],
+                                                                "outgoing" : process[tag][i]["bpmn2:outgoing"] == undefined ? [] : process[tag][i]["bpmn2:outgoing"] },
+                                              "bounds" : bounds["dc:Bounds"]["$"],
+                                              "metaDiagram" : bounds["$"]
+                                            } : "insert into tblstorage(id,metadata,shapetype,hash,connectors, bounds, metadiagram) values ('" + mongoose.Types.ObjectId() + "','" + process[tag][i]["$"] + "','" + tag+ "','" + hashInfo + "','" +  process[tag][i]["bpmn2:incoming"] +","+process[tag][i]["bpmn2:outgoing"] + "','" + bounds + "','" + result["bpmn2:definitions"]["process"][0]["bpmndi:BPMNDiagram"][0]["$"] + "')";
+
+      if(server != "couchbase")
+        temp.push(data);
+      else  
+        temp[_id] = {"value" : data};
     });
   });
   cache.put("mapping", temp);
   return temp;
 }
 
-function processDataCB(result, processTag){
-  var jsonFile =  JSON.stringify(result)  ;
-  result = JSON.parse(jsonFile);
-  var temp = {};
-
-  var process = result["bpmn2:definitions"]["process"][0]["bpmn2:process"][0];
-  _.each(processTag, function(tag){
-    _.each(process[tag], function(t,i){
-      var shapes = result["bpmn2:definitions"]["process"][0]["bpmndi:BPMNDiagram"][0]["bpmndi:BPMNPlane"][0]["bpmndi:BPMNShape"];
-      var bounds = [];
-      var hashInfo = Date.now();
-      kHash(tag + Date.now(), hashInfo);
-
-      if(shapes.length > 0 )
-        _.each(shapes, function(shape){
-          _.each(process[tag], function(jtag){
-            if(jtag["$"]["id"] == shape["$"]["bpmnElement"] )
-              bounds = shape;
-          })
-        });
-
-      var data = {
-        //"_id": mongoose.Types.ObjectId(),
-        "id" : Guid.raw(),
-        "metaData" : process[tag][i]["$"],
-        "shapeType" : tag,
-        "hash" : hashInfo,
-        "connectors" : {  "incoming" : process[tag][i]["bpmn2:incoming"] == undefined ? [] : process[tag][i]["bpmn2:incoming"],
-                          "outgoing" : process[tag][i]["bpmn2:outgoing"] == undefined ? [] : process[tag][i]["bpmn2:outgoing"] },
-        "bounds" : bounds["dc:Bounds"]["$"],
-        "metaDiagram" : bounds["$"]
-      }
-
-      var key = mongoose.Types.ObjectId();     
-      temp[key] = {"value" : data};
-    });
-  });
-  cache.put("mapping", temp);
-  return temp;
-} 
-
-
-
-var processDataCassandra = function(result, processTag){
-  var jsonFile =  JSON.stringify(result)  ;
-  result = JSON.parse(jsonFile);
-  var temp = [];
-
-  var process = result["bpmn2:definitions"]["process"][0]["bpmn2:process"][0];
-  _.each(processTag, function(tag){
-    _.each(process[tag], function(t,i){
-      var shapes = result["bpmn2:definitions"]["process"][0]["bpmndi:BPMNDiagram"][0]["bpmndi:BPMNPlane"][0]["bpmndi:BPMNShape"];
-      var bounds = [];
-      var hashInfo = Date.now();
-      kHash(tag + Date.now(), hashInfo);
-
-      if(shapes.length > 0 )
-        _.each(shapes, function(shape){
-          _.each(process[tag], function(jtag){
-            if(jtag["$"]["id"] == shape["$"]["bpmnElement"] )
-              bounds = shape;
-          })
-        });
-
-      var data= "insert into tblstorage(id,metadata,shapetype,hash,connectors, bounds, metadiagram) values ('" + mongoose.Types.ObjectId() + "','" + process[tag][i]["$"] + "','" + tag+ "','" + hashInfo + "','" +  process[tag][i]["bpmn2:incoming"] +","+process[tag][i]["bpmn2:outgoing"] + "','" + bounds + "','" + result["bpmn2:definitions"]["process"][0]["bpmndi:BPMNDiagram"][0]["$"] + "')";                    
-      temp.push(data);
-    });
-  });
-  cache.put("mapping", temp);
-  return temp;              
+function getType(server){
+  switch (server){    
+    case "couchbase":
+      return {};
+    default:
+      return [];
+  }
 }
-
 
 var server = http.createServer(onRequest);
-server.listen(8080);
+server.listen(8081);
 console.log("> NODE.JS STARTED");
